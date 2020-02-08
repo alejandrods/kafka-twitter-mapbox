7b26a3f5360165ffc088c79a593ec4e5d361451e
alejandrods <alejandro.diaz.santos@gmail.com>
2020-02-08 02:21:06 +0100

    Add mobile support Add loading page

---
+++ Front_End/static/main_mobile.js
@@ -0,0 +1,14 @@
+// Load Loading Animation
+var lottieAnimation = bodymovin.loadAnimation({
+  container: document.getElementById('anim'),
+  path: '../static/resources/Twitter_Anim.json',
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+})
+
+// Redirect Function
+function redirectIt(obj){
+    var goToLink = obj.getAttribute("href");
+    window.location.href=goToLink;
+}
