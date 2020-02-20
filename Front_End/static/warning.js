c9e601e60baa61221ec8724454eb41cc516cc2b8
alejandrods <alejandro.diaz.santos@gmail.com>
2020-02-20 12:54:10 +0100

    Add Popup

--- Front_End/static/warning.js
+++ Front_End/static/warning.js
@@ -1,4 +1,4 @@
-function make_tour() {
+function warning_connection() {
     const tour = new Shepherd.Tour({
       tourName: 'debug-tour',
       useModalOverlay: true,
@@ -12,6 +12,33 @@ function make_tour() {
     tour.addStep({
       title: 'Please, bear in mind',
       text: `Sometimes, it takes some time to connect the app with our Kafka Server to get the coordinates`,
+      buttons: [
+        {
+          action() {
+            return this.next();
+          },
+          text: 'Close'
+        }
+      ],
+      id: 'Welcome'
+    });
+
+    tour.start();
+}
+
+function warning_following() {
+    const tour = new Shepherd.Tour({
+      tourName: 'debug-tour',
+      useModalOverlay: true,
+      defaultStepOptions: {
+        classes: 'c-onboarding-modal',
+        scrollTo: true,
+        scrollToHandler: this.scrollToHandler
+      },
+      disableScroll: true
+    });
+    tour.addStep({
+      text: '<img alt="img_github" src="../static/resources/Example_Progress.png" width=150px" height="100px">',
       buttons: [
         {
           action() {
